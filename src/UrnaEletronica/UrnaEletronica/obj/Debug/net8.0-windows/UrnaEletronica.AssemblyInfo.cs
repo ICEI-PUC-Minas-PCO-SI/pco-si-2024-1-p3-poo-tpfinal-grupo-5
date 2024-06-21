b3911b8a007939790402a1c7005a7633bee4d7c5
Matheus Canuto <70597769+MatheusCanuto07@users.noreply.github.com>
2024-06-20 22:01:01 -0300

    Apagando models

--- src/UrnaEletronica/UrnaEletronica/obj/Debug/net8.0-windows/UrnaEletronica.AssemblyInfo.cs
+++ src/UrnaEletronica/UrnaEletronica/obj/Debug/net8.0-windows/UrnaEletronica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68caf6137d2ed71c652966100359ad5b863b04f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
