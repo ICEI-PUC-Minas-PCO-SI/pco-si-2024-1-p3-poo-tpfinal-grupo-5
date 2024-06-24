073e70caa93e69795984b961856ec771728ca5b2
Matheus Canuto <70597769+MatheusCanuto07@users.noreply.github.com>
2024-06-23 22:13:27 -0300

    Limpa de arquivos

--- src/UrnaEletronica/UrnaEletronica/obj/Debug/net8.0-windows/UrnaEletronica.AssemblyInfo.cs
+++ src/UrnaEletronica/UrnaEletronica/obj/Debug/net8.0-windows/UrnaEletronica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d8ef25ec788eebb14587a14fbeaa7d225c93a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f5e55154fe046e2f9e46e311811566210e7301")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrnaEletronica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
